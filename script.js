d9e658d053a84b4dfe5aab4fc9b63dda3352bf40
kokilaAravindhan <125110809+kokilaAravindhan@users.noreply.github.com>
2023-03-25 01:22:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+
+/* Find the culprit and invoke the alert
+ 
+alert("I’m invoked!");*/
+ //--------------------------------------------------------------------------------------
+/* Explain the below how it works
+
+alert("I'm JavaScript!");
+alert('Hello') // this line is not having semicolon
+               //this line does not having ""
+alert(`Wor
+ ld`)         //this line does not having ""
+alert(3 +
+1
++ 2);          //the concatination of line is improper*/
+//-----------------------------------------------------------------------------------------
+/*  Fix the below to alert Guvi geek 
+
+let admin=9, fname=10.5; 
+fname = "Guvi";
+lname = "geek"
+admin = fname+" "+lname;
+alert( admin );*/
+//------------------------------------------------------------------------------------------
+let fname=10.5,name=9; 
+fname = "Guvi";
+let lname = "geek"
+name = fname+" "+lname;
+alert( 'hello ${name}' );
